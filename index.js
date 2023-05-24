9dd975786bbd1f35962eb95f9943b3541096c504
JessiAraujo <jessii.aaraujo@gmail.com>
2023-05-24 17:39:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function getValue(){
+
+    var value= document.getElementById('name').value;
+   
+    document.getElementById('result').innerHTML = "Bem vindo," + value + "!";
+
+
+}
+
+function removeValue(){
+    document.getElementById('name').value = "";
+    document.getElementById('result').innerHTML = "";
+}
